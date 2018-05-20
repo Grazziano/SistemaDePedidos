9215ac81b0c15228b2a57c29b34ff8ea4cc4a9be
Grazziano <grazzianofagundes@gmail.com>
2018-05-20 09:21:11 -0300

    Projeto criado

---
+++ src/main/java/com/grazzianofagundes/sistemadepedidos/SistemadepedidosApplication.java
@@ -0,0 +1,12 @@
+package com.grazzianofagundes.sistemadepedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadepedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadepedidosApplication.class, args);
+	}
+}
